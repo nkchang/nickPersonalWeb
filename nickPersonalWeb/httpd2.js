b749b82cbbb8af2a6c94fb61b9b5b6de0295ee7e
Nick Lukuei Chang <nkchang@gmail.com>
2016-11-28 18:55:33 +0800

    Add YUI3 sample, modify HTTPDd
    modify HTTPd code, add content type, create new port 8889

--- nickPersonalWeb/httpd2.js
+++ nickPersonalWeb/httpd2.js
@@ -3,13 +3,20 @@
 
 //建立一個httpd物件
 var httpd = require("http");
+
+var server8889 = httpd.createServer();
+server8889.listen(8889);
+
 httpd.createServer(function(request, response) {
   //response.writeHead(200, {"content-type": "plain-text"});
   //加入Unicode support
-  response.writeHead(200, {"content-type": "plain-text"});
-  response.writeHead(200, {"content-language": "utf-8"});
-  response.write('<html><body>');
-  response.write('<p1>Hello,World!測試成功</p1>');
+  //如果只要顯示純文字檔案就用這個摟.
+  //response.writeHead(200, {"content-type": "plain-text"});
+  //使用HTML
+  response.writeHead(200, {"content-type": "text/html"});
+  response.writeHead(200, {"content-language": "Unicode"});
+  response.write('<html><title>Nick personalWeb HTTPd</title><body>');
+  response.write('<h1>Hello,World!測試成功</h1>');
   response.write("</body></html>");
   response.end();
 }).listen(8888);
---
+++ nickPersonalWeb/nodejs-dom.js
@@ -0,0 +1,6 @@
+var YUI = require("yui3").YUI;
+
+YUI().use("nodejs-dom", "dom", function(Y){
+//Where code goes to
+Y.log("Start...");
+});
